45d434d88127698995daf2c095585e550bd5847b
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-10-29 11:07:12 +0800

    nix: setup dev environment for C

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  outputs = { self, nixpkgs, devenv, flake-utils, ... } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              languages.c.enable = true;
+              packages = with pkgs; [
+                clang-tools
+              ];
+            }
+          ];
+        };
+      });
+}
